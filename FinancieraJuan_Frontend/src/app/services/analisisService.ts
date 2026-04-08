0c81db54fee1f109628e6e62188fdc03231c0db6
Aldair <aldairbroncanosoltelo@gmail.com>
2026-04-07 21:55:26 -0500

    Se modifico ruta crossOrigin

--- FinancieraJuan_Frontend/src/app/services/analisisService.ts
+++ FinancieraJuan_Frontend/src/app/services/analisisService.ts
@@ -6,7 +6,7 @@ import { Observable } from 'rxjs';
   providedIn: 'root',
 })
 export class AnalisisService {
-  private apiUrl = 'https://financierajuan_backend.railway.app/';
+  private apiUrl = 'https://financierajuan_backend.railway.app';
 
   constructor(private http: HttpClient) {}
 
