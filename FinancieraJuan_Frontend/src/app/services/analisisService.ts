1bb026a7eeaac3ec6c5926df018aecf20ddaa2e2
Aldair <aldairbroncanosoltelo@gmail.com>
2026-04-07 20:47:05 -0500

    frontend: se modifico apiUrl para conectar con el backend

--- FinancieraJuan_Frontend/src/app/services/analisisService.ts
+++ FinancieraJuan_Frontend/src/app/services/analisisService.ts
@@ -6,7 +6,7 @@ import { Observable } from 'rxjs';
   providedIn: 'root',
 })
 export class AnalisisService {
-  private apiUrl = 'http://localhost:8080/api/calcular';
+  private apiUrl = 'https://financierajuan_backend.railway.app/';
 
   constructor(private http: HttpClient) {}
 
